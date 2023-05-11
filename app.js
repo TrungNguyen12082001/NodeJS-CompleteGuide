eebcb6588920c19d6de3e718114218570be1535d
TrungNguyen <nguyentattrung1208@gmail.com>
2023-05-11 21:55:53 +0700

    Using Environment Variables

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ const multer = require("multer");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const MONGODB_URI = `${process.env.MONGODB_URI}`;
+console.log(process.env.NODE_ENV);
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.wjkvrnd.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -116,7 +118,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
